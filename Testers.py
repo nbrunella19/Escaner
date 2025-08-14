21f96c05000711cdf0e9db73181c747a06fc7303
nbrunella19 <nicolasbunella@gmail.com>
2025-08-14 14:12:27 -0300

    Actualización de Multímetro

--- Testers.py
+++ Testers.py
@@ -1,6 +1,8 @@
 from Instrumental.MI6010D import MI60100
 from Instrumental.Scanner import ScannerInti
-
+from Instrumental.HP3458A import HP3458A
+from Instrumental.HP34401 import HP34401A
+from Instrumental.HP34420 import HP34420A
 
 """
 # Ejemplo de uso Query MI60100
@@ -9,9 +11,17 @@ bridge.local_unlock()
 print("Estado:", bridge.query())
 bridge.close()
 """
+Mul_HP3458A = HP3458A("GPIB0::26::INSTR")
+valor=Mul_HP3458A.identify()
+print("Identificación del HP3458A:", valor) 
+Mul_HP3458A.close()
 
+"""
+# Pruebas en el scanner
 scanner = ScannerInti()  # Esto abre la conexión GPIB
 #scanner.SetearCanal(scanner.DireccionGPIB, ScannerInti.SALIDA_1, 5)
 #scanner.ResetGeneral(scanner.DireccionGPIB)
-print("Valor leído:",scanner.Ver(scanner.DireccionGPIB, ScannerInti.SALIDA_1))
-#print("Valor leído:", valor)
+#print("Valor leído:",scanner.Ver(scanner.DireccionGPIB, ScannerInti.SALIDA_1))
+"""
+
+
